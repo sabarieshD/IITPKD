f5197eeb28c39b979127ab0e8263d955580e6250
sabarieshD <109064056+sabarieshD@users.noreply.github.com>
2022-12-04 13:10:38 +0530

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,62 @@
+/*const age= 43;
+const drive = age >= 16 ? 'allowed': 'not allowed';
+console.log(`you are ${drive} drive`);
+
+let drive2;
+if (age>=18){
+    drive2='allowed';
+}else{
+    drive2='not allowed'
+}
+console.log(drive2);*/
+
+/*function lemonJuice(water, sugar){
+    const juice=`Take ${water} ml of water.Then add  ml of lemon essence and add ${sugar} large cubes of sugar.Finally stir it well.`;
+    
+    return juice;
+}
+const fruitjuice= lemonJuice=(250, 2);
+    console.log(fruitjuice);
+    function calcAge1(birthyear){
+        return 2037-birthyear;
+
+    }
+    const age1 = calcAge1 (1991);
+    console.log(age1);
+
+    const calcAge2= function(birthyear){
+        return 2037 - birthyear;
+
+    }
+    const age2= calcAge2(1991);
+    console.log(age1,age2);
+    const yearsuntileretirement= (birthyear,firstname) => {
+        const age=2022-birthyear;
+        const retirementage= 65-age;
+        
+        if (retirementage<=25) {
+            console.log(`${firstname} age is ${age} and years left for his retirement is ${retirementage} years.`)
+        }else{
+            console.log(`${firstname} retaires in ${retirementage} years `)
+        }
+    }
+   console.log(yearsuntileretirement(1991,'Jonas'));
+   console.log(yearsuntileretirement(1980,'bob'));
+ 
+const checkWinner = ((44,23,71),(54,65,49))=>
+    eval (avgdolhins=(x+y+z)/3);
+    eval (avgkoalas=(x+y+z)/3);
+   return checkWinner;
+
+
+console.log(checkwinner((85,54,41),(23,34,27))); 
+const friends=['john','peter','sarav','karthick'];
+const popped= friends.pop();
+console.log(popped);
+const javascript= friends.pop();
+console.log(javascript);
+let rep =1;
+while(rep<=10){
+    console.log(`lifting weights ${rep}`);
+    rep++;
+} */
